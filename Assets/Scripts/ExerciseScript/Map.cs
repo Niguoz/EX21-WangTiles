ba097240234fb90908f6abc1f35d543210d62e7c
Niguoz <nicola.pellegrini96@tiscali.it>
2022-03-15 13:32:57 +0100

    WangTiles

--- Assets/Scripts/ExerciseScript/Map.cs
+++ Assets/Scripts/ExerciseScript/Map.cs
@@ -7,31 +7,40 @@ public class Map
     [System.Serializable]
     public struct Size
     {
-        public int widht;
-        public int height;
+        public uint widht;
+        public uint height;
     }
 
     private WangTile[,] _map;
     private uint _widht, _height;
+
+    public Map(Size size) : this(size.widht, size.height) { }
+
     public Map(uint width, uint height)
     {
         _widht = width;
         _height = height;
 
-        _map = new WangTile[width, height];
+        _map = new WangTile[height, width];
 
         GenerateMap();
     }
 
     public void GenerateMap()
     {
-        for (uint row = 0; row < _widht; row++)
+        for (uint row = 0; row < _height; row++)
         {
-            for (uint col = 0; col < _height; col++)
+            for (uint col = 0; col < _widht; col++)
             {
+                //TODO : create generator
                 var val = Random.Range(1, 16);
-                _map[row, col] = new WangTile((uint) val);
+                _map[row, col] = new WangTile((uint)val);
             }
         }
+    }
+
+    public WangTile GetTile(uint row, uint col)
+    {
+        return _map[row, col];
     }
 }
--- Assets/Scripts/ExerciseScript/TileGenerator.cs
+++ Assets/Scripts/ExerciseScript/TileGenerator.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class TileGenerator : MonoBehaviour
 {
-   public Map.Size size;
+    public Map.Size size;
+    public float tileSize = 32;
+    private Map _map;
 
-   void Start()
-   {
-       Generate();
-   }
+    void Start()
+    {
+        Generate();
+    }
 
-   public void Generate()
-   {}
+    public void Generate()
+    {
+        _map = new Map(size.widht, size.height);
+        _map.GenerateMap();
+
+        for (int row = 0; row < size.height; row++)
+        {
+            var output = "";
+            for (int col = 0; col < size.widht; col++)
+            {
+                var val = _map.GetTile((uint)row, (uint)col).Value;
+                output += val + " - ";
+
+                var go = new GameObject("Tile " + row + " " + col);
+                var sr = go.AddComponent<SpriteRenderer>();
+                var spriteSheet = Resources.LoadAll("Wang");
+                Debug.Log(spriteSheet[1]);
+                var sprite = (Sprite)spriteSheet[val + 1];
+                sr.sprite = sprite;
+                go.transform.position = new Vector3(col, row, 0);
+            }
+            Debug.Log(output);
+        }
+    }
 }
